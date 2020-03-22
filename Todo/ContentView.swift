95df4435473bc932f40c0dff9fc008c5f49457ca
Camiel Koomen <Ckoomen@gmail.com>
2020-03-22 16:21:30 +0100

    Initial Commit

---
+++ Todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by Camiel Koomen on 22/03/2020.
+//  Copyright © 2020 Camiel Koomen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
