4f8213a457c09139a67379a026acdd78b0fac5cc
Camiel <ckoomen@gmail.com>
2021-02-01 22:46:59 +0100

    ZStack, stack of cards

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -10,31 +10,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-		VStack {
-			HStack {
-				VStack(alignment: .leading){
-							Text("Hello, Wolrd")
-								.font(.title)
-								.foregroundColor(Color("accent"))
-								.fontWeight(.semibold)
-								Text("Tryouts")
-				}
+		ZStack {
+			VStack{
 				Spacer()
-				Image("Logo1")
 			}
-			.padding(.horizontal, 20)
-			.padding(.top, 20)
-			Spacer()
-			Image("Card1")
+			.frame(width: 300, height: 220)
+			.background(Color("background3"))
+			.cornerRadius(20)
+			.shadow(radius: 20 )
+			.offset(x:0, y: -20)
 			
-			.resizable()
-				.aspectRatio(contentMode: .fill)
-				.frame(width: 300, height: 110, alignment: .top)
+			VStack {
+				HStack {
+					VStack(alignment: .leading){
+								Text("Hello, Wolrd")
+									.font(.title)
+									.foregroundColor(Color("accent"))
+									.fontWeight(.semibold)
+									Text("Tryouts")
+					}
+					Spacer()
+					Image("Logo1")
+				}
+				.padding(.horizontal, 20)
+				.padding(.top, 20)
+				Spacer()
+				Image("Card1")
+				
+				.resizable()
+					.aspectRatio(contentMode: .fill)
+					.frame(width: 300, height: 110, alignment: .top)
+			}
+			.frame(width: 340.0, height: 220.0)
+			.background(Color("background2"))
+			.cornerRadius(20)
+			.shadow(radius: 20)
 		}
-		.frame(width: 340.0, height: 220.0)
-		.background(Color("background2"))
-		.cornerRadius(20)
-		.shadow(radius: 20)
 	}
 }
 
