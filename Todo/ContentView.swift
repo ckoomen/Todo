e4825cb2e6c0a416a0db90af016fac91c9b01b88
Camiel <ckoomen@gmail.com>
2021-02-03 21:18:45 +0100

    More cards, components and content views  components

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -11,40 +11,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
 		ZStack {
-			VStack{
-				Spacer()
-			}
-			.frame(width: 300, height: 220)
-			.background(Color("background3"))
-			.cornerRadius(20)
-			.shadow(radius: 20 )
-			.offset(x:0, y: -20)
+			BackCardView()
+				.offset(x:0, y: -40)
+				.scaleEffect(0.9)
+				.rotationEffect(Angle (degrees: 5))
 			
-			VStack {
-				HStack {
-					VStack(alignment: .leading){
-								Text("Hello, Wolrd")
-									.font(.title)
-									.foregroundColor(Color("accent"))
-									.fontWeight(.semibold)
-									Text("Tryouts")
-					}
-					Spacer()
-					Image("Logo1")
-				}
-				.padding(.horizontal, 20)
-				.padding(.top, 20)
-				Spacer()
-				Image("Card1")
-				
-				.resizable()
-					.aspectRatio(contentMode: .fill)
-					.frame(width: 300, height: 110, alignment: .top)
-			}
-			.frame(width: 340.0, height: 220.0)
-			.background(Color("background2"))
-			.cornerRadius(20)
-			.shadow(radius: 20)
+			BackCardView()
+				.offset(x:0, y: -20)
+				.scaleEffect(0.95)
+				.rotationEffect(Angle(degrees: 5))
+			
+			CardView()
 		}
 	}
 }
@@ -53,4 +30,47 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct CardView: View {
+	var body: some View {
+		VStack {
+			HStack {
+				VStack(alignment: .leading){
+					Text("Hello, Wolrd")
+						.font(.title)
+						.foregroundColor(Color("accent"))
+						.fontWeight(.semibold)
+					Text("Tryouts")
+				}
+				Spacer()
+				Image("Logo1")
+			}
+			.padding(.horizontal, 20)
+			.padding(.top, 20)
+			Spacer()
+			Image("Card1")
+				
+				.resizable()
+				.aspectRatio(contentMode: .fill)
+				.frame(width: 300, height: 110, alignment: .top)
+		}
+		.frame(width: 340.0, height: 220.0)
+		.background(Color("background2"))
+		.cornerRadius(20)
+		.shadow(radius: 20)
+	}
+}
+
+struct BackCardView: View {
+	var body: some View {
+		VStack{
+			Spacer()
+		}
+		.frame(width: 340, height: 220)
+		.background(Color("background3"))
+		.cornerRadius(20)
+		.shadow(radius: 20)
+	
+	}
 }
