5c9520b0db36b5e010f57613328c3a6c36c86913
Camiel <ckoomen@gmail.com>
2021-02-03 22:10:41 +0100

    Creating components, using effects properties

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -11,17 +11,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
 		ZStack {
-			BackCardView()
-				.offset(x:0, y: -40)
-				.scaleEffect(0.9)
-				.rotationEffect(Angle (degrees: 5))
+			TitleView()
+				.blur(radius: 20)
 			
 			BackCardView()
+				.background(Color("background2"))
+				.cornerRadius(20)
+				.shadow(radius: 20).offset(x:0, y: -40)
+				.scaleEffect(0.9)
+				.rotationEffect(.degrees(10))
+				.rotation3DEffect(Angle(degrees:10),
+								  axis: (x: 10.0, y: 0, z: 0 ))
+				.blendMode(.hardLight)
+			
+			BackCardView()
+				.background(Color("background3"))
+				.cornerRadius(20)
+				.shadow(radius: 20)
 				.offset(x:0, y: -20)
 				.scaleEffect(0.95)
 				.rotationEffect(Angle(degrees: 5))
+				.rotation3DEffect(Angle(degrees:5),
+								  axis: (x: 10.0, y: 0, z: 0 ))
+				.blendMode(.hardLight)
 			
 			CardView()
+				.blendMode(.hardLight)
+			
+			BottomCardView()
+				.blur(radius: 20)
+			
 		}
 	}
 }
@@ -56,7 +75,7 @@ struct CardView: View {
 				.frame(width: 300, height: 110, alignment: .top)
 		}
 		.frame(width: 340.0, height: 220.0)
-		.background(Color("background2"))
+		.background(Color("background1"))
 		.cornerRadius(20)
 		.shadow(radius: 20)
 	}
@@ -68,9 +87,46 @@ struct BackCardView: View {
 			Spacer()
 		}
 		.frame(width: 340, height: 220)
-		.background(Color("background3"))
-		.cornerRadius(20)
-		.shadow(radius: 20)
+
 	
+	}
+}
+
+struct TitleView: View {
+	var body: some View {
+		VStack {
+			HStack {
+				Text("A Big Screentitle")
+					.font(.largeTitle)
+					.fontWeight(.bold)
+				Spacer()
+			}
+			.padding()
+			Image("Background1")
+			Spacer()
+		}
+	}
+}
+
+struct BottomCardView: View {
+	var body: some View {
+		VStack(spacing: 20) {
+			Rectangle()
+				.frame(width: 40, height: 5)
+				.cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+				.opacity(0.1)
+			Text("This certificate is proof that Camiel is starting with someSwiftUI courses")
+				.multilineTextAlignment(.center)
+				.font(.subheadline)
+				.lineSpacing(4)
+			Spacer()
+		}
+		.padding(.top, 8)
+		.padding(.horizontal, 20)
+		.frame(maxWidth: .infinity)
+		.background(Color.white)
+		.cornerRadius(30)
+		.shadow(radius: 20)
+		.offset(x: 0, y: 600)
 	}
 }
