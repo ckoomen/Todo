317526668494333a339931f7704ce0219df100fb
Camiel <ckoomen@gmail.com>
2021-02-08 22:20:40 +0100

    Added Drag Animation

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -23,6 +23,7 @@ struct ContentView: View {
 				.cornerRadius(20)
 				.shadow(radius: 20)
 				.offset(x:0, y: show ? -400 : -40)
+				.offset(x: viewState.width, y: viewState.height)
 				.scaleEffect(0.9)
 				.rotationEffect(.degrees(show ? 0 : 10))
 				.rotation3DEffect(Angle(degrees: 10),
@@ -35,6 +36,7 @@ struct ContentView: View {
 				.cornerRadius(20)
 				.shadow(radius: 20)
 				.offset(x:0, y: show ? -200 : -20)
+				.offset(x: viewState.width, y: viewState.height)
 				.scaleEffect(0.95)
 				.rotationEffect(Angle.degrees(show ? 0 : 5))
 				.rotation3DEffect(Angle(degrees:5),
@@ -45,16 +47,18 @@ struct ContentView: View {
 			CardView()
 				.offset(x: viewState.width, y: viewState.height)
 				.blendMode(.hardLight)
-				.animation(.spring())
+				.animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
 				.onTapGesture {
 					self.show.toggle()
 				}
 				.gesture(
 					DragGesture().onChanged { value in
 						self.viewState = value.translation
+						self.show = true
 					}
 					.onEnded { value in
 						self.viewState = .zero
+						self.show = false
 						}
 					)
 			
