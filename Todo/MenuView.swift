b9d13d09eecf96d0354f26dd21c78003e3568052
Camiel <ckoomen@gmail.com>
2021-02-11 22:57:24 +0100

    Finished lesson, added new file with menu items, used SF symbols and fonts

--- Todo/MenuView.swift
+++ Todo/MenuView.swift
@@ -10,17 +10,23 @@ import SwiftUI
 
 struct MenuView: View {
     var body: some View {
-		VStack (spacing: 16){
-			MenuRow(title: "Account", icon: "gear")
-			MenuRow(title: "Billing", icon: "creditcard")
-			MenuRow(title: "third", icon: "scribble")
-		}
-		.frame(maxWidth: .infinity)
-		.frame(height: 300)
-		background(Color.white)
-			.clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-			.shadow(radius: 30 )
+		VStack {
+			Spacer()
+			
+			VStack (spacing: 16){
+				MenuRow(title: "Account", icon: "gear")
+				MenuRow(title: "Billing", icon: "creditcard")
+				MenuRow(title: "Sign out", icon: "person.crop.circle")
+			}
+			.frame(maxWidth: .infinity)
+			.frame(height: 300)
+			.background(Color.white)
+				.clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+				.shadow(radius: 30)
 			.padding(.horizontal, 30)
+		}
+		.padding(.bottom, 30)
+		
     }
 }
 
