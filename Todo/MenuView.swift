0405c3bfd5d2fffd27bbfb04f4968e479cc0f59e
Camiel <ckoomen@gmail.com>
2021-02-15 21:36:01 +0100

    9th section done, color literals and creating interfaces with frames.

--- Todo/MenuView.swift
+++ Todo/MenuView.swift
@@ -14,16 +14,38 @@ struct MenuView: View {
 			Spacer()
 			
 			VStack (spacing: 16){
+				Text("Camiel - 50% complete")
+					.font(.caption)
+				
+				Color.white
+					.frame(width: 38, height: 6, alignment: .leading)
+					.cornerRadius(3.0)
+					.frame(width: 130, height: 6, alignment: .leading)
+					.background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.08))
+					.cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+					.padding()
+					.frame(width: 150, height: 24 )
+					.background(Color.black.opacity(0.1))
+					.cornerRadius(12)
+				
 				MenuRow(title: "Account", icon: "gear")
 				MenuRow(title: "Billing", icon: "creditcard")
 				MenuRow(title: "Sign out", icon: "person.crop.circle")
 			}
 			.frame(maxWidth: .infinity)
 			.frame(height: 300)
-			.background(Color.white)
+			.background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9960784314, green: 0.9960784314, blue: 0.9960784314, alpha: 1)), Color(#colorLiteral(red: 0.9490196078, green: 0.9490196078, blue: 0.9490196078, alpha: 1))]), startPoint: .top, endPoint: .bottom))
 				.clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-				.shadow(radius: 30)
+			.shadow(color: Color.black.opacity(0.2), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
 			.padding(.horizontal, 30)
+			.overlay(
+				Image("Avatar")
+					.resizable()
+					.aspectRatio(contentMode: .fill)
+					.frame(width: 60, height: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+					.clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+					.offset(y: -150)
+			)
 		}
 		.padding(.bottom, 30)
 		
@@ -43,6 +65,7 @@ struct MenuRow: View {
 	var body: some View {
 		HStack (spacing: 16) {
 			Image(systemName: icon)
+				.foregroundColor(Color("card2"))
 				.font(.system(size: 20, weight: .light))
 				.imageScale(.large)
 				.frame(width: 32, height: 32, alignment: .center)
