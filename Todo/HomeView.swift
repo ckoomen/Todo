4fa59761d631ad42c52251a78eeae5b35a38ad28
Camiel <ckoomen@gmail.com>
2021-02-16 22:19:39 +0100

    Lesson 13 done, using Scrollviews and Repeat

--- Todo/HomeView.swift
+++ Todo/HomeView.swift
@@ -22,7 +22,20 @@ struct HomeView: View {
 				AvatarView(showProfile: $showProfile)
 			}
 			.padding(.horizontal)
+			.padding(.leading, 14)
 			.padding(.top, 30)
+			
+			
+			
+			ScrollView(.horizontal, showsIndicators: false) {
+				HStack(spacing:30) {
+					ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+						SectionView()
+					}
+				}
+				.padding(30)
+				.padding(.bottom, 30)
+			}
 			
 			Spacer()
 		}
@@ -33,4 +46,33 @@ struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
 		HomeView(showProfile: .constant(false))
     }
+}
+
+struct SectionView: View {
+	var body: some View {
+		VStack {
+			HStack (alignment: .top){
+				Text("Prototype designs in SwiftUI")
+					.font(.system(size: 24, weight: .bold))
+					.frame(width: 160, alignment: .leading)
+				Spacer()
+				Image("Logo1")
+			}
+			Text("18 Sections".uppercased())
+				.frame(maxWidth: .infinity, alignment: .leading)
+			
+			Image("Card1")
+				.resizable()
+				.aspectRatio(contentMode: .fit)
+				.frame(width: 210)
+			
+		}
+		.padding(.top, 20)
+		.padding(.horizontal, 20)
+		
+		.frame(width: 275, height: 275)
+		.background(Color("card2"))
+		.cornerRadius(30)
+		.shadow(color: Color("card2").opacity(0.3), radius: 2, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
+	}
 }
