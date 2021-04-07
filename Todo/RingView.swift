ce783c6930053b040a06f9de32a012563c164660
Camiel <ckoomen@gmail.com>
2021-04-07 21:39:21 +0200

    Small lesson making components

--- Todo/RingView.swift
+++ Todo/RingView.swift
@@ -9,28 +9,41 @@
 import SwiftUI
 
 struct RingView: View {
+	var color1 = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+	var color2 = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+	var width: CGFloat = 300
+	var height: CGFloat = 300
+	var percent: CGFloat = 22
+
+	
+	
+	
     var body: some View {
-		ZStack {
-			Circle()
-				.stroke(Color.black.opacity(0.1),
-						style: StrokeStyle(lineWidth: 5))
-				.frame(width: 44, height: 44, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+		let multiplier = width / 44
+		let progress = 1 - (percent / 100)
+		return ZStack {
+			
 			
 			Circle()
-				.trim(from: 0.2, to: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+				.stroke(Color.black.opacity(0.1),
+						style: StrokeStyle(lineWidth: 5 * multiplier))
+				.frame(width: width, height: height, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+			
+			Circle()
+				.trim(from: progress, to: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
 				.stroke(
-					LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)), Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1))]), startPoint: .topTrailing, endPoint: .bottomLeading),
+					LinearGradient(gradient: Gradient(colors: [Color(color1), Color(color2)]), startPoint: .topTrailing, endPoint: .bottomLeading),
 						
-					style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20,0], dashPhase: 0))
+					style: StrokeStyle(lineWidth: 5 * multiplier, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20,0], dashPhase: 0))
 				.rotationEffect(.init(degrees: 90))
 				.rotation3DEffect(
 					Angle(degrees: 180),
 					axis: (x: 1.0, y: 0.0, z: 0.0))
-				.frame(width: 44, height: 44, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-				.shadow(color: (Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).opacity(0.1)), radius: 3, x: 0, y: 3)
+				.frame(width: width, height: height, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+				.shadow(color: (Color(color2).opacity(0.1)), radius: 3, x: 0, y: 3 * multiplier)
 			
-			Text("82%")
-				.font(.subheadline)
+			Text("\(Int(percent))%")
+				.font(.system(size: 14 * multiplier))
 				.fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
 		}
     }
